9703b10f3d843a9668494a6b6ff664eed67460ff
admin <admin@qq.com>
2022-02-12 11:21:51 +0800

    third commit

--- src/main/java/com/atguigu/git/gittest.java
+++ src/main/java/com/atguigu/git/gittest.java
@@ -4,5 +4,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git test");
         System.out.println("hello git test");
+        System.out.println("hello git test");
     }
 }
